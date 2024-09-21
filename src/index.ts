bfc3f63a438c03eb8242c0294f1b85bcf4e77364
Steve <stevedsimkins@gmail.com>
2024-09-21 16:01:28 -0400

    init

---
+++ src/index.ts
@@ -0,0 +1,29 @@
+import { Hono } from "hono";
+import { PGlite } from "@electric-sql/pglite";
+
+const app = new Hono();
+const db = new PGlite();
+
+app.get("/", async (c) => {
+	await db.exec(`
+     CREATE TABLE IF NOT EXISTS todo (
+       id SERIAL PRIMARY KEY,
+       task TEXT,
+       done BOOLEAN DEFAULT false
+     );
+     INSERT INTO todo (task, done) VALUES ('Install PGlite from NPM', true);
+     INSERT INTO todo (task, done) VALUES ('Load PGlite', true);
+     INSERT INTO todo (task, done) VALUES ('Create a table', true);
+     INSERT INTO todo (task, done) VALUES ('Insert some data', true);
+     INSERT INTO todo (task) VALUES ('Update a task');
+   `);
+
+	const ret = await db.query(`
+  SELECT * from todo WHERE id = 1;
+`);
+	console.log(ret.rows);
+
+	return c.text("Hello Hono!");
+});
+
+export default app;
