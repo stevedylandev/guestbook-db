884558754fb9ba72af24f07c34b0b52b9258f1f7
Steve <stevedsimkins@gmail.com>
2024-09-21 16:19:56 -0400

    WIP

--- src/index.ts
+++ src/index.ts
@@ -5,24 +5,27 @@ const app = new Hono();
 const db = new PGlite();
 
 app.get("/", async (c) => {
-	await db.exec(`
-     CREATE TABLE IF NOT EXISTS todo (
-       id SERIAL PRIMARY KEY,
-       task TEXT,
-       done BOOLEAN DEFAULT false
-     );
-     INSERT INTO todo (task, done) VALUES ('Install PGlite from NPM', true);
-     INSERT INTO todo (task, done) VALUES ('Load PGlite', true);
-     INSERT INTO todo (task, done) VALUES ('Create a table', true);
-     INSERT INTO todo (task, done) VALUES ('Insert some data', true);
-     INSERT INTO todo (task) VALUES ('Update a task');
-   `);
-
 	const ret = await db.query(`
   SELECT * from todo;
 `);
 
 	return c.json(ret.rows);
+});
+
+app.get("/create", async (c) => {
+	await db.exec(`
+      CREATE TABLE IF NOT EXISTS todo (
+        id SERIAL PRIMARY KEY,
+        task TEXT,
+        done BOOLEAN DEFAULT false
+      );
+      INSERT INTO todo (task, done) VALUES ('Install PGlite from NPM', true);
+      INSERT INTO todo (task, done) VALUES ('Load PGlite', true);
+      INSERT INTO todo (task, done) VALUES ('Create a table', true);
+      INSERT INTO todo (task, done) VALUES ('Insert some data', true);
+      INSERT INTO todo (task) VALUES ('Update a task');
+    `);
+	return c.text("done");
 });
 
 app.get("/update", async (c) => {
